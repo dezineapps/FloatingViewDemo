c080f6c4276acfbce30033b874af78b58aee0b4a
wangxin20050524 <wangxin20050524@163.com>
2013-07-16 11:29:29 +0800

    1, add the receiver for monitor the running state 2, handle the conflict between touch event and click event

--- src/com/mike/floating/FloatingActivity.java
+++ src/com/mike/floating/FloatingActivity.java
@@ -2,96 +2,24 @@ package com.mike.floating;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.Toast;
 
 public class FloatingActivity extends Activity {
-	/** Called when the activity is first created. */
 
-//	private WindowManager mWindowManager = null;
-//	private WindowManager.LayoutParams mLayoutParams = null;
-//
 	private FloatingView mFloatingView = null;
-//	private boolean mIsFloatingViewRemoved = false;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		Button btnClick = (Button) findViewById(R.id.click);
-		btnClick.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				Toast.makeText(getApplicationContext(), "Click done!", Toast.LENGTH_SHORT).show();
-			}
-		});
-//		createView();
-		mFloatingView = new FloatingView(getApplicationContext());
+		mFloatingView = new FloatingView(this);
 		mFloatingView.show();
-
-	}
-
-//	private void createView() {
-//		mFloatingView = new FloatingView(getApplicationContext());
-//		mWindowManager = (WindowManager) getApplicationContext()
-//				.getSystemService("window");
-//		mLayoutParams = ((FloatingApplication) getApplication())
-//				.getMywmParams();
-//
-//		/**
-//		 * ���¶���WindowManager.LayoutParams��������� ������;�ɲο�SDK�ĵ�
-//		 */
-//		mLayoutParams.type = LayoutParams.TYPE_PHONE; // ����window type
-//		mLayoutParams.format = PixelFormat.RGBA_8888; // ����ͼƬ��ʽ��Ч��Ϊ����͸��
-//
-//		// ����Window flag
-//		mLayoutParams.flags = LayoutParams.FLAG_NOT_TOUCH_MODAL
-//				| LayoutParams.FLAG_NOT_FOCUSABLE;
-//		/*
-//		 * �����flags���Ե�Ч����ͬ������
-//		 * ����ɴ������������κ��¼�,ͬʱ��Ӱ�������¼���Ӧ��
-//		 * wmParams.flags=LayoutParams.FLAG_NOT_TOUCH_MODAL |
-//		 * LayoutParams.FLAG_NOT_FOCUSABLE | LayoutParams.FLAG_NOT_TOUCHABLE;
-//		 */
-//
-//		mLayoutParams.gravity = Gravity.LEFT | Gravity.TOP; // ������������Ͻ�
-//		// ����Ļ���Ͻ�Ϊԭ�㣬����x��y��ʼֵ
-//		mLayoutParams.x = 0;
-//		mLayoutParams.y = 0;
-//
-//		// ������ڳ������
-//		mLayoutParams.width = 40;
-//		mLayoutParams.height = 40;
-//
-//		// ��ʾmyFloatViewͼ��
-//		mWindowManager.addView(mFloatingView, mLayoutParams);
-//	}
-
-	@Override
-	protected void onStop() {
-		super.onStop();
-		if (null != mFloatingView) {
-			mFloatingView.dismiss();
-		}
-//		mWindowManager.removeView(mFloatingView);
-//		mIsFloatingViewRemoved = true;
-		
 	}
 
 	@Override
-	protected void onResume() {
-		super.onResume();
+	protected void onDestroy() {
 		if (null != mFloatingView) {
-			mFloatingView.show();
+			mFloatingView.release();
 		}
-//		if (null != mWindowManager && null != mFloatingView
-//				&& mIsFloatingViewRemoved) {
-//			mWindowManager.addView(mFloatingView, mLayoutParams);
-//			mIsFloatingViewRemoved = false;
-//		}
+		super.onDestroy();
 	}
-
 }
--- src/com/mike/floating/FloatingView.java
+++ src/com/mike/floating/FloatingView.java
@@ -1,15 +1,25 @@
 package com.mike.floating;
 
+import android.app.Activity;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.PixelFormat;
+import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.LinearLayout;
+
+import com.mike.floating.service.RunningStateService;
 
 /*
  * @author wangxin
@@ -17,30 +27,36 @@ import android.widget.LinearLayout;
  * @date June 24, 2013
  */
 
-public class FloatingView extends LinearLayout {
+public class FloatingView extends LinearLayout implements OnClickListener {
 	private static final String TAG = "FloatingView";
 	
-	private static final int MUSIC_BAR_MODE_STARDARD = 0;
-	private static final int MUSIC_BAR_MODE_MINI = 0;
-	private static final int MUSIC_BAR_MODE_FULLSCREEN = 0;
-	
+	private static final int MOVE_DISTANCE_MIN = 10;
+
 	private float mTouchStartX;
 	private float mTouchStartY;
 	private float mRawX;
 	private float mRawY;
+	
+	private float mRawStartX = 0;
+	private float mRawStartY = 0;
 
 	private WindowManager mWindowManager;
-	private WindowManager.LayoutParams mLayoutParams;
+	private WindowManager.LayoutParams mWindowLayoutParams;
+	private int mLastWindowHeight = 0;
 	private View mContentView = null;
-	private boolean mIsFloatingViewRemoved = true;
+
+	private Button mBtnToFullscreen;
+	private Button mBtnToMini;
+	private Button mBtnToStardard;
 	
+	private boolean mIsShowing = false;
+	private HomeReceiver mHomeReceiver;
+	
+	private int mScreenHeight;
+	private int mStatusBarHeight;
+
 	public FloatingView(Context context) {
 		super(context);
-		init();
-	}
-
-	public FloatingView(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
 		init();
 	}
 
@@ -50,89 +66,206 @@ public class FloatingView extends LinearLayout {
 	}
 
 	private void init() {
-		mWindowManager = (WindowManager) getContext().getApplicationContext()
-				.getSystemService("window");
+		initScreenWidthHeihgt();
+		initWindowLayoutParams();
+		prepareForAddView();
+		prepareForServiceReveiver();
+	}
 
-		mLayoutParams = ((FloatingApplication) getContext()
-				.getApplicationContext()).getWindowManagerLayoutParams();
+	/**
+	 * 
+	 */
+	@SuppressWarnings("deprecation")
+	private void prepareForAddView() {
 		LayoutInflater inflater = LayoutInflater.from(getContext());
 		mContentView = inflater.inflate(R.layout.test, null);
-		addView(mContentView);
-		initLayoutParams();
+		mContentView.setOnClickListener(this);
+		mBtnToFullscreen = (Button) mContentView
+				.findViewById(R.id.btn_fullscreen);
+		mBtnToMini = (Button) mContentView.findViewById(R.id.btn_mini);
+		mBtnToFullscreen.setOnClickListener(this);
+		mBtnToMini.setOnClickListener(this);
+	    mBtnToStardard = (Button) mContentView.findViewById(R.id.btn_stardard);
+	    mBtnToStardard.setOnClickListener(this);
+		addView(mContentView, LinearLayout.LayoutParams.FILL_PARENT,
+				LinearLayout.LayoutParams.FILL_PARENT);
+		mWindowManager.addView(this, mWindowLayoutParams);
+	}
+	
+	private void initScreenWidthHeihgt() {
+		DisplayMetrics metrics = new DisplayMetrics();
+		mWindowManager = (WindowManager) getContext().getApplicationContext()
+				.getSystemService(Context.WINDOW_SERVICE);
+		mWindowManager.getDefaultDisplay().getMetrics(metrics);
+		mScreenHeight = metrics.heightPixels;
+		Activity activity = (Activity) getContext();
+		Rect rect= new Rect();  
+		activity.getWindow().getDecorView().getWindowVisibleDisplayFrame(rect);  
+		mStatusBarHeight = rect.top; 
+		
+		Log.d(TAG, "initScreenWidthHeihgt mScreenHeight = " + mScreenHeight);
+		Log.d(TAG, "initScreenWidthHeihgt mStatusBarHeight = " + mStatusBarHeight);
 	}
 
-	private void initLayoutParams() {
-		// int WindowManager LayoutParams
-		mLayoutParams.type = WindowManager.LayoutParams.TYPE_PHONE; // window
-																	// type
-		mLayoutParams.format = PixelFormat.RGBA_8888;
-
-		// int WindowManager Flags
-		mLayoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+	@SuppressWarnings("deprecation")
+	private void initWindowLayoutParams() {
+		mWindowLayoutParams = ((FloatingApplication) getContext()
+				.getApplicationContext()).getWindowManagerLayoutParams();
+ 
+		mWindowLayoutParams.type = WindowManager.LayoutParams.TYPE_PHONE;  
+		mWindowLayoutParams.format = PixelFormat.RGBA_8888;
+		mWindowLayoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
 				| WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
-		/*
-		 * wmParams.flags=LayoutParams.FLAG_NOT_TOUCH_MODAL |
-		 * LayoutParams.FLAG_NOT_FOCUSABLE | LayoutParams.FLAG_NOT_TOUCHABLE;
-		 */
+		mWindowLayoutParams.gravity = Gravity.LEFT | Gravity.TOP;
 
-		mLayoutParams.gravity = Gravity.LEFT | Gravity.TOP;
-
-		mLayoutParams.x = 0;
-		mLayoutParams.y = 0;
-
-		mLayoutParams.width = 400;
-		mLayoutParams.height = 80;
+		mWindowLayoutParams.x = 0;
+		mWindowLayoutParams.y = 0;
+		
+        //Note: the following parameters is the key for width and height of display window
+		mWindowLayoutParams.width = WindowManager.LayoutParams.FILL_PARENT;  
+		mWindowLayoutParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
+		
+		mLastWindowHeight = mWindowLayoutParams.height;
 	}
 
+	private void prepareForServiceReveiver() {
+		getContext().startService(
+				new Intent(getContext(), RunningStateService.class));
+		mHomeReceiver = new HomeReceiver();
+		IntentFilter filter = new IntentFilter();
+		filter.addAction(RunningStateService.ACTION_STATE_HOME);
+		filter.addAction(RunningStateService.ACTION_STATE_INNER);
+		filter.addAction(RunningStateService.ACTION_STATE_OTHER);
+		getContext().getApplicationContext().registerReceiver(mHomeReceiver,
+				filter);
+	}
+	
 	public void show() {
-		if (null != mWindowManager && null != mContentView
-				&& mIsFloatingViewRemoved) {
+		if (null != mContentView && !mIsShowing) {
 			Log.d(TAG, "show()");
-			mWindowManager.addView(this, mLayoutParams);
-			mIsFloatingViewRemoved = false;
+			mWindowLayoutParams.height = mLastWindowHeight;
+			mWindowManager.updateViewLayout(this, mWindowLayoutParams);
+			mContentView.setVisibility(View.VISIBLE);
+			mIsShowing = true;
 		}
 	}
 
-	public void dismiss() {
+	public void hide() {
+		if (null != mContentView && mIsShowing) {
+			Log.d(TAG, "hide()");
+			mWindowLayoutParams.height = 0;
+			mWindowManager.updateViewLayout(this, mWindowLayoutParams);
+			mContentView.setVisibility(View.GONE);
+			mIsShowing = false;
+		}
+	}
+	
+	public void release() {
 		if (null != mWindowManager && null != mContentView) {
-			Log.d(TAG, "dismiss()");
+			Log.d(TAG, "release()");
 			mWindowManager.removeView(this);
-			mIsFloatingViewRemoved = true;
+			mIsShowing = false;
+			getContext().getApplicationContext().unregisterReceiver(
+					mHomeReceiver);
 		}
 	}
 
+	
 	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-
-		// default position
+	public boolean onInterceptTouchEvent(MotionEvent event) {
 		mRawX = event.getRawX();
-		mRawY = event.getRawY() - 25; // remove the height of notification bar
-		Log.i(TAG, "RawX = " + mRawX + " RawY = " + mRawY);
+		mRawY = event.getRawY() - mStatusBarHeight; // remove the height of notification bar
 		switch (event.getAction()) {
 		case MotionEvent.ACTION_DOWN:
-			// catch the touch point
 			mTouchStartX = event.getX();
 			mTouchStartY = event.getY();
-
-			Log.i(TAG, "startX = " + mTouchStartX + " startY = " + mTouchStartY);
-
+			mRawStartX = mRawX;
+			mRawStartY = mRawY;
+			break;
+		case MotionEvent.ACTION_MOVE:
+			if (isNeedUpdateViewPosition()) {
+				return true;
+			}
+			break;
+		case MotionEvent.ACTION_UP:
+			if (isNeedUpdateViewPosition()) {
+				return true;
+			}
+			break;
+		}
+		return super.onInterceptTouchEvent(event);
+	}
+	
+	private boolean isNeedUpdateViewPosition() {
+		return Math.abs(mRawX - mRawStartX) > MOVE_DISTANCE_MIN || Math.abs(mRawY - mRawStartY) > MOVE_DISTANCE_MIN;
+	}
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		Log.d(TAG, "onTouchEvent() ");
+		mRawX = event.getRawX();
+		mRawY = event.getRawY() - mStatusBarHeight;  
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
 			break;
 		case MotionEvent.ACTION_MOVE:
 			updateViewPosition();
 			break;
-
 		case MotionEvent.ACTION_UP:
 			updateViewPosition();
-			mTouchStartX = mTouchStartY = 0;
 			break;
 		}
 		return true;
 	}
 
 	private void updateViewPosition() {
-		mLayoutParams.x = (int) (mRawX - mTouchStartX);
-		mLayoutParams.y = (int) (mRawY - mTouchStartY);
-		mWindowManager.updateViewLayout(this, mLayoutParams);
+		mWindowLayoutParams.x = (int) (mRawX - mTouchStartX);
+		mWindowLayoutParams.y = (int) (mRawY - mTouchStartY);
+		mWindowManager.updateViewLayout(this, mWindowLayoutParams);
+	}
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.btn_fullscreen:
+			mWindowLayoutParams.width = WindowManager.LayoutParams.FILL_PARENT;
+			mWindowLayoutParams.height = WindowManager.LayoutParams.FILL_PARENT;
+			mLastWindowHeight = mWindowLayoutParams.height;
+			mWindowManager.updateViewLayout(this, mWindowLayoutParams);
+			break;
+		case R.id.btn_mini:
+			mWindowLayoutParams.width = WindowManager.LayoutParams.WRAP_CONTENT;
+			mWindowLayoutParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
+			mWindowLayoutParams.x = 0;
+			mLastWindowHeight = mWindowLayoutParams.height;
+			mWindowManager.updateViewLayout(this, mWindowLayoutParams);
+			break;
+		case R.id.btn_stardard:
+			mWindowLayoutParams.width = WindowManager.LayoutParams.FILL_PARENT;
+			mWindowLayoutParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
+			mWindowLayoutParams.x = 0;
+			mLastWindowHeight = mWindowLayoutParams.height;
+			mWindowManager.updateViewLayout(this, mWindowLayoutParams);
+			break;
+		}
+	}
+
+	class HomeReceiver extends BroadcastReceiver {
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			String action = intent.getAction();
+			Log.d(TAG, "HomeReceiver onReceive()  action = " + action);
+			if (RunningStateService.ACTION_STATE_HOME.equals(action)) {
+				hide();
+			} else if (RunningStateService.ACTION_STATE_INNER.equals(action)) {
+				show();
+			} else if (RunningStateService.ACTION_STATE_OTHER.equals(action)) {
+				hide();
+			}
+		}
+
 	}
 
 }
